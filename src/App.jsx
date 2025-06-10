f1575257c003a9c6019bf5ad95956b3ff7d91cfc
mark abwoga <markabwoga@gmail.com>
2025-06-10 03:27:52 +0300

    feat(login): add InstructorLogIn component and update LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import InstructorLogIn from './components/InstructorLogIn';
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/InstructorLogIn" element={<InstructorLogIn />} />
       </Routes>
     </Router>
   );
---
+++ src/components/InstructorLogIn.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react';
+import InstructorPanel from './instructor/InstructorPanel'
+import { Link } from 'react-router-dom';
+
+export default function InstructorLogIn() {
+          const [isLoggedIn, setIsLoggedIn] = useState(false);
+          const [user, setUser] = useState({
+            username: "", 
+            password: "",
+          });
+        
+          function handleChange(e) {
+            const { name, value } = e.target;
+            setUser(prev => ({
+              ...prev,
+              [name]: value
+            }));
+          }
+        
+          function handleSubmit(e) {
+            e.preventDefault(); 
+            const { username, password } = user;
+        
+            if (username === "Instructor" && password === "Instructor") {
+              setIsLoggedIn(true);
+              console.log('Success');
+            } else {
+              console.log('Login failed');
+            }
+          }
+        
+            if (isLoggedIn) {
+                return <InstructorPanel />
+            
+          }
+        
+          return (
+            <div>
+              <h1>Instructor Login Page</h1>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    value={user.username}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleChange}
+                  />
+                  <button type="submit">Log In</button>
+                 <Link to="/LoginPage">SignIn as a Student</Link>
+                </form>
+            </div>
+          );
+        }
