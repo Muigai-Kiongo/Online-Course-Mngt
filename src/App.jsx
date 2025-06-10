28266cc42b260c7d8db89a29458108c3e1b75c0c
Emkei <emkei@localhost.localdomain>
2025-06-11 02:31:35 +0300

    structured mock data component,mapped the data and refined on the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import LoginPage from './components/LoginPage';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import EnrollmentPage from "./components/student/EnrollmentPage";
+import InstructorPanel from "./components/instructor/InstructorPanel";
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" exact element={<HomePage />} />
         <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/enrollPage" element={<EnrollmentPage />} />
+        <Route path="/instructor-panel" element={<InstructorPanel />} />
       </Routes>
     </Router>
   );
 }
-
