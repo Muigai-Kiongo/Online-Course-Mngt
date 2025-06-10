79359986bf7b9acdd0f75f8488a4281909be32a4
Emkei <emkei@localhost.localdomain>
2025-06-11 02:54:45 +0300

    resolved a conflicted component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,59 +1,59 @@
-import React, { useState } from 'react';
-import CourseContainer from './student/CourseContainer';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import HomePage from "./HomePage";
 
 export default function LoginPage() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({
-    username: "", 
+    username: "",
     password: "",
   });
 
   function handleChange(e) {
     const { name, value } = e.target;
-    setUser(prev => ({
+    setUser((prev) => ({
       ...prev,
-      [name]: value
+      [name]: value,
     }));
   }
 
   function handleSubmit(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     const { username, password } = user;
 
     if (username === "Student" && password === "Student") {
       setIsLoggedIn(true);
-      console.log('Success');
+      console.log("Success");
     } else {
-      console.log('Login failed');
+      console.log("Login failed");
     }
   }
 
-    if (isLoggedIn) {
-    return <CourseContainer />;
+  if (isLoggedIn) {
+    return <HomePage />;
   }
 
   return (
     <div>
       <h1>Students Login Page</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={user.username}
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={user.password}
-            onChange={handleChange}
-          />
-          <button type="submit">Log In</button>
-          <Link to="/InstructorLogIn">SignIn as an Instructor</Link>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Log In</button>
+        <Link to="/InstructorLogIn">SignIn as an Instructor</Link>
+      </form>
     </div>
   );
 }
