b088aeeb332a884c9be88198ed2d04b210398d2b
Emkei <emkei@localhost.localdomain>
2025-06-12 13:47:08 +0300

    conflict resolve

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
-import InstructorLogIn from './components/InstructorLogIn';
-import InstructorPanel from './components/instructor/InstructorPanel';
-import Courses from './components/student/Courses';
-import CourseDetailPage from './components/student/CourseDetailsPage';
-import ProtectedRoutes from './components/ProtectedRoutes';
+import InstructorLogIn from "./components/InstructorLogIn";
+import InstructorPanel from "./components/instructor/InstructorPanel";
+import CourseDetailPage from "./components/student/CourseDetailsPage";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 import EnrollmentPage from "./components/student/EnrollmentPage";
 
-
-
 export default function App() {
-  console.log("App rendered")
+  console.log("App rendered");
   return (
     <Router>
       <Header />
@@ -23,12 +20,11 @@ export default function App() {
         <Route path="/InstructorLogIn" element={<InstructorLogIn />} />
 
         <Route element={<ProtectedRoutes />}>
-            <Route path="/InstructorPanel" element={<InstructorPanel />} />
-            <Route path="/CourseDetailsPage" element={<CourseDetailPage />} />
-            <Route path="/Courses" element={<Courses />} />
-            <Route path="/EnrollmentPage" element={<EnrollmentPage />} />
-            <Route path="/course/:id" element={<CourseDetailsPage />} />
+          <Route path="/instructor-panel" element={<InstructorPanel />} />
+          <Route path="/enrollPage" element={<EnrollmentPage />} />
+          <Route path="/course/:id" element={<CourseDetailPage />} />
         </Route>
+      </Routes>
     </Router>
   );
 }
