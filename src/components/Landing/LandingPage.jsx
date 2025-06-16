8db988f14af79419ee18c62a5d1879e9e7cd116f
mark abwoga <markabwoga@gmail.com>
2025-06-16 20:48:28 +0300

    install and configure tailwindcss and its packages

---
+++ src/components/Landing/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header'
+
+export default function LandingPage() {
+  return (
+    <div>
+        <div class="logo-container">
+        <img src="/SOC_logo.svg" alt="SOC logo" />
+        <span class="school-name">School of Code</span>
+        </div>
+    </div>
+  )
+}
